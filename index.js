e3d23b2deba19b9d47b3cfb22578d159f2513ffb
SuyashDhiman856 <139106983+SuyashDhiman856@users.noreply.github.com>
2024-09-14 14:16:59 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // peerServer.js
-const express = require("express");
-const http = require("http");
-const { ExpressPeerServer } = require("peer");
+const express = require('express');
+const http = require('http');
+const { ExpressPeerServer } = require('peer');
 
 // Initialize Express app
 const app = express();
@@ -11,13 +11,22 @@ const server = http.createServer(app);
 
 // Initialize the PeerJS server with debug enabled
 const peerServer = ExpressPeerServer(server, {
+  path: '/',
+  allow_discovery: true,
   debug: true,
 });
 
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
+});
+
 // Use PeerJS server on '/peerjs' path
-app.use("/peerjs", peerServer);
+app.use('/peerjs', peerServer);
 
 // Start the server on port 3001
-server.listen(3001, () => {
-  console.log("PeerJS server is running on port 3001");
+server.listen(9000, () => {
+  console.log('PeerJS server is running on port 3001');
 });
